17af33a5e62b8f16566b764b2086f13f807fe57c
Shadow-M0narch <robin.schmid.business@gmail.com>
2022-02-21 11:48:19 +0100

    played around with strings

--- Examples/strings.py
+++ Examples/strings.py
@@ -3,6 +3,13 @@ print("Giraffe\"Acadamy")
 print("Giraffe\Acadamy")
 
 phrase = "Giraffe Acadamy"
-print(phrase + "is cool")
+print(phrase + " is cool")
 print(phrase.lower())
 print(phrase.upper())
+print(phrase.upper().isupper())
+print(len(phrase))
+
+print(type(len(phrase)))
+itemsphrase = str(len(phrase))
+print("There are " + itemsphrase + " characters")
+print("There are " + str(len(phrase)) + " characters")
