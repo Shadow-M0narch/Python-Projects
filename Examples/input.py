955b5ba127feedb33e206579f354da5ca63d0b99
Shadow-M0narch <robin.schmid.business@gmail.com>
2022-02-21 12:20:55 +0100

    message

--- Examples/input.py
+++ Examples/input.py
@@ -1,5 +1,5 @@
-from asyncore import loop
-
-
 name = input("Enter your name: ")
-print("Hello " + name + "!")
+if name == "Robin":
+    print("Hello King " + name + "!")
+else:
+    print("Hello " + name + "!")
