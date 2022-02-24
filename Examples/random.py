a2b92697739391187de2e1fba1d740061bec2a25
Shadow-M0narch <robin.schmid.business@gmail.com>
2022-02-24 13:05:22 +0100

    random

--- Examples/random.py
+++ Examples/random.py
@@ -1,4 +1,33 @@
-def fibo(n):
-    if n in [0, 1]:
-        return n
-print (fibo)
+from tkinter.messagebox import YES
+
+
+male = ["male", "boy", "man"]
+female = ["female", "girl", "woman"]
+stupid_loop_answers = ["yes", "no"]
+yes = "yes"
+no = "no"
+def user_gender():
+    gender = input("Enter your gender: ")
+    if gender in male:
+        print("You are a male")
+    elif gender in female:
+        print("You are a female")
+    elif gender == "stupid":
+        print("I told you to tell me your gender")
+        return user_gender()
+    elif gender == "no":
+        print("Yes.")
+        return stupid_loop()
+    else:
+        print("ERROR 404: Gender does not exist")
+        return user_gender()
+
+def stupid_loop(sla):
+    input()
+    if sla in stupid_loop_answers:
+
+
+
+    
+
+user_gender()
