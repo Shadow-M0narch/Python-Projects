d7ce2275c7ef70aa195d86efbec60dc36b9fc479
Shadow-M0narch <robin.schmid.business@gmail.com>
2022-03-09 11:26:32 +0100

    wdw

--- Examples/random.py
+++ Examples/random.py
@@ -1,21 +1,63 @@
-male = ["male", "boy", "man"]
-female = ["female", "girl", "woman"]
-stupid_loop_answers = ["yes", "no"]
-yes = "yes"
-no = "no"
-def user_gender():
-    gender = input("Enter your gender: ")
-    if gender in male:
-        print("You are a male")
-    elif gender in female:
-        print("You are a female")
-    elif gender == "stupid":
-        print("I told you to tell me your gender")
-        return user_gender()
-    elif gender == "no":
-        print("Yes.")
-        return user_gender()
-    else:
-        print("ERROR 404: Gender does not exist")
-        return user_gender()
-user_gender()
+from tkinter import *
+from tokenize import String
+
+class MyApp:
+    def __init__(self, root):
+        root.title("Fibonacci")
+        root.iconbitmap('./assets/monokuma.ico')
+
+        menu = Menu(root)
+        item = Menu(menu)
+
+        def close():
+            root.destroy()
+            root.quit()
+
+        item.add_command(label='Exit', command=close)
+        menu.add_cascade(label='File', menu=item)
+
+        root.config(menu=menu)
+
+        window_width = 300
+        window_height = 300
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        center_x = int(screen_width/2 - window_width / 2)
+        center_y = int(screen_height/2 - window_height / 2)
+
+        root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+        label1 = Label(root, text="Your Input:")
+        label1.pack()
+
+        # Dictionary Method
+        # label["text"] = "Hello Everyone."
+        # label ["font"] = ("Courier", 20)
+
+        #Configure Method
+        # label.configure(text="Hello Everyone.", font=("Courier", 20))
+
+        self.entry_text = StringVar()
+        entry = Entry(root, textvariable=self.entry_text)
+        self.label_text = StringVar()
+        label2 = Label(root, textvariable=self.label_text)
+
+        label2.pack()
+        entry.pack()
+
+        
+        button = Button(root, text="Submit", command=self.press_button)
+        button.pack()
+
+
+    def press_button(self):
+        text = self.entry_text.get()
+        self.label_text.set(text)
+        
+
+root = Tk()
+
+MyApp(root)
+
+root.mainloop()
+
